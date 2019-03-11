05c8f216fbc3975110145e99e577e9705913283b
prmiles <prmiles@ncsu.edu>
2019-03-11 13:38:18 -0400

    added basic unit test for last method

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -70,3 +70,12 @@ class GaussWeights(unittest.TestCase):
 # --------------------------
 def exponential_function(x):
     return np.exp(2*x)
+
+
+class RiemannSumRLFD(unittest.TestCase):
+    def test_rs_rl_fd(self):
+        alpha = 0.9
+        gridpoints = np.linspace(0.0, 1.0, 64)
+        df = utilities.riemann_sum_RL_FD(f=exponential_function,
+                                         gridpoints=gridpoints, alpha=alpha)
+        self.assertTrue(float(df), msg='Expect output to be a float')
