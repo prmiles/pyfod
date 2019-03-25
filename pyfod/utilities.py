78adc2cde26394ce9ae32ee33bf2153fcab22d09
prmiles <prmiles@ncsu.edu>
2019-03-25 17:16:52 -0400

    removed GQ methods from utilities module and test_utilities module

--- pyfod/utilities.py
+++ pyfod/utilities.py
@@ -6,64 +6,6 @@ Created on Fri Jul 19 10:14:07 2018
 @author: prmiles
 """
 
-import numpy as np
-
-
-# *********************************************
-# ---------------
-def base_gauss_points():
-
-    # base points
-    gpts = np.zeros([4])
-    gpts[0] = (1/2) - np.sqrt(15+2*np.sqrt(30))/(2*np.sqrt(35))
-    gpts[1] = (1/2) - np.sqrt(15-2*np.sqrt(30))/(2*np.sqrt(35))
-    gpts[2] = (1/2) + np.sqrt(15-2*np.sqrt(30))/(2*np.sqrt(35))
-    gpts[3] = (1/2) + np.sqrt(15+2*np.sqrt(30))/(2*np.sqrt(35))
-    return gpts
-
-
-# ---------------
-def base_gauss_weights(h):
-
-    # define the Gauss weights for a four point quadrature rule
-    w = np.zeros([4, len(h)])
-    w[0, :] = 49*h[:]/(12*(18 + np.sqrt(30)))
-    w[1, :] = 49*h[:]/(12*(18 - np.sqrt(30)))
-    w[2, :] = w[1, :]
-    w[3, :] = w[0, :]
-    return w
-
-
-# ---------------
-def interval_gauss_points(base_gpts, N, h, low_lim):
-    # determines the Gauss points for all N intervals.
-    Gpts = np.zeros([4*N, len(h)])
-    for gct in range(N):
-        for ell in range(4):
-            Gpts[(gct)*4 + ell, :] = (gct)*h[:] + base_gpts[ell]*h[:] + low_lim
-    return Gpts
-
-
-# ---------------
-def gauss_points(N, h, low_lim):
-    # base points
-    gpts = base_gauss_points()
-    # determines the Gauss points for all N intervals.
-    Gpts = interval_gauss_points(gpts, N, h, low_lim)
-    return Gpts
-
-
-# ---------------
-def gauss_weights(N, h):
-    # determine the Gauss weights for a four point quadrature rule
-    w = base_gauss_weights(h)
-    # copy the weights to form a vector for all N intervals
-    weights = w.copy()
-    for gct in range(N-1):
-        weights = np.concatenate((weights, w))
-    return weights
-
-
 # ---------------
 def riemann_sum_RL_FD(f, gridpoints, alpha):
     n = len(gridpoints)
--- test/test_utilities.py
+++ test/test_utilities.py
@@ -10,63 +10,6 @@ import numpy as np
 import unittest
 from pyfod import utilities
 
-
-# --------------------------
-class BaseGaussPoints(unittest.TestCase):
-
-    def test_base_gauss_points(self):
-        gpts = utilities.base_gauss_points()
-        mlgpts = np.array([0.069431844202974, 0.330009478207572,
-                           0.669990521792428, 0.930568155797026])
-        self.assertEqual(gpts.size, 4, msg='Expect 4 elements')
-        self.assertEqual(gpts.shape, (4,), msg='Expect shape = (4,)')
-        self.assertTrue(np.allclose(gpts, mlgpts),
-                        msg=str('Expect arrays to match: {} neq {}'.format(
-                                gpts,
-                                mlgpts)))
-
-
-# --------------------------
-class BaseGaussWeights(unittest.TestCase):
-
-    def test_base_gauss_weights(self):
-        gwts = utilities.base_gauss_weights(h=np.array([0.1]))
-        mlgwts = np.array([[0.017392742256873, 0.032607257743127,
-                            0.032607257743127, 0.017392742256873]]).T
-        self.assertEqual(gwts.size, 4, msg='Expect 4 elements')
-        self.assertEqual(gwts.shape, (4, 1), msg='Expect shape = (4,1)')
-        self.assertTrue(np.allclose(gwts, mlgwts),
-                        msg=str('Expect arrays to match: {} neq {}'.format(
-                                gwts, mlgwts)))
-
-
-# --------------------------
-class IntervalGaussPoints(unittest.TestCase):
-
-    def test_interval_gauss_points(self):
-        igp = utilities.interval_gauss_points(
-                base_gpts=utilities.base_gauss_points(),
-                N=4, h=np.array([0.1, 0.2]), low_lim=0.)
-        self.assertEqual(igp.shape, (4*4, 2), msg='Expect shape = (16,2)')
-
-
-# --------------------------
-class GaussPoints(unittest.TestCase):
-
-    def test_gauss_points(self):
-        gpoints = utilities.gauss_points(N=4,
-                                         h=np.array([0.1, 0.2]), low_lim=0.)
-        self.assertEqual(gpoints.shape, (4*4, 2), msg='Expect shape = (16,2)')
-
-
-# --------------------------
-class GaussWeights(unittest.TestCase):
-
-    def test_gauss_weights(self):
-        gweights = utilities.gauss_weights(N=4, h=np.array([0.1, 0.2]))
-        self.assertEqual(gweights.shape, (4*4, 2), msg='Expect shape = (16,2)')
-
-
 # --------------------------
 def exponential_function(x):
     return np.exp(2*x)
