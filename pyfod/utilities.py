2c0f5f7127eda71c047b74ef760e79681154a159
prmiles <prmiles@ncsu.edu>
2019-04-08 10:05:14 -0400

    updated format

--- pyfod/utilities.py
+++ pyfod/utilities.py
@@ -6,6 +6,8 @@ Created on Fri Jul 19 10:14:07 2018
 @author: prmiles
 """
 
+import sys
+
 
 def check_alpha(alpha):
     try:
@@ -14,3 +16,18 @@ def check_alpha(alpha):
         print('Invalid value! The value of alpha cannot \
               be 1.0. alpha = {}:\n\t{}'.format(alpha, err))
         raise SystemExit
+
+
+def check_function(f, default_f):
+    if f is None:
+        f = default_f
+    if f is None:
+        sys.exit('No function defined... provide function f')
+    return f
+
+
+def check_singularity(singularity, finish):
+    if singularity is None:
+        return finish
+    else:
+        return singularity
