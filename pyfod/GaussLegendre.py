967ec7bb20e05fcbe4d524ce26124e2be13e4b95
prmiles <prmiles@ncsu.edu>
2019-04-08 09:57:53 -0400

    cleaned up files and testing

--- pyfod/GaussLegendre.py
+++ pyfod/GaussLegendre.py
@@ -5,33 +5,43 @@ Created on Mon Mar 11 17:12:41 2019
 
 @author: prmiles
 """
-import sys
+
 import numpy as np
-from .utilities import check_alpha
+from pyfod.utilities import check_alpha
+from pyfod.utilities import check_function, check_singularity
 
 
 class GaussLegendre:
 
-    def __init__(self, N=5, start=0.0, finish=1.0, alpha=0.0, f=None):
+    def __init__(self, N=5, start=0.0, finish=1.0,
+                 alpha=0.0, f=None, singularity=None):
         self.description = 'Gaussian-Legendre Quadrature'
         check_alpha(alpha)
         h = (finish - start)/N
         self.alpha = alpha
+        self.f = f
         self.finish = finish
-        self.points = self.gauss_points(N=N, h=h, start=start)
-        self.weights = self.gauss_weights(N=N, h=h)
+        self.singularity = check_singularity(singularity, self.finish)
+        self.points = self._gauss_points(N=N, h=h, start=start)
+        self.weights = self._gauss_weights(N=N, h=h)
         self.initial_weights = self.weights.copy()
         self.update_weights(alpha=alpha)
 
+    def update_weights(self, alpha=None):
+        if alpha is None:
+            alpha = self.alpha
+        self.alpha = alpha
+        # update weights based on alpha
+        self.weights = self.initial_weights*(
+                self.singularity - self.points)**(-alpha)
+
     def integrate(self, f=None):
-        if f is None:
-            f = self.f
-        if f is None:
-            sys.exit('No function defined... provide function f')
+        f = check_function(f, self.f)
+        self.f = f
         return (self.weights*f(self.points)).sum()
 
     @classmethod
-    def base_gauss_points(cls):
+    def _base_gauss_points(cls):
 
         # base points
         gpts = np.zeros([4])
@@ -42,7 +52,7 @@ class GaussLegendre:
         return gpts
 
     @classmethod
-    def base_gauss_weights(cls, h):
+    def _base_gauss_weights(cls, h):
         # define the Gauss weights for a four point quadrature rule
         w = np.zeros([4])
         w[0] = 49*h/(12*(18 + np.sqrt(30)))
@@ -52,7 +62,7 @@ class GaussLegendre:
         return w
 
     @classmethod
-    def interval_gauss_points(cls, base_gpts, N, h, start):
+    def _interval_gauss_points(cls, base_gpts, N, h, start):
         # determines the Gauss points for all N intervals.
         Gpts = np.zeros([4*N])
         for gct in range(N):
@@ -61,29 +71,21 @@ class GaussLegendre:
                                        + base_gpts[ell]*h + start)
         return Gpts
 
-    def gauss_points(self, N, h, start):
+    def _gauss_points(self, N, h, start):
         # base points
-        gpts = self.base_gauss_points()
+        gpts = self._base_gauss_points()
         # determines the Gauss points for all N intervals.
-        Gpts = self.interval_gauss_points(gpts, N, h, start)
+        Gpts = self._interval_gauss_points(gpts, N, h, start)
         return Gpts
 
-    def gauss_weights(self, N, h):
+    def _gauss_weights(self, N, h):
         # determine the Gauss weights for a four point quadrature rule
-        w = self.base_gauss_weights(h)
+        w = self._base_gauss_weights(h)
         # copy the weights to form a vector for all N intervals
         weights = w.copy()
         for gct in range(N-1):
             weights = np.concatenate((weights, w))
         return weights
-
-    def update_weights(self, alpha=None):
-        if alpha is None:
-            alpha = self.alpha
-        self.alpha = alpha
-        # update weights based on alpha
-        self.weights = self.initial_weights*(
-                self.finish - self.points)**(-alpha)
 
 
 if __name__ == '__main__':  # pragma: no cover
