fc39dc78fd66b055a60341e6483ae25dbcd45fd6
prmiles <prmiles@ncsu.edu>
2019-04-08 09:27:40 -0400

    updated attributes to have hidden methods
    User should only see functions:
    - update_weights
    - integrate

--- pyfod/RiemannSum.py
+++ pyfod/RiemannSum.py
@@ -5,9 +5,10 @@ Created on Mon Mar 25 15:00:01 2019
 
 @author: prmiles
 """
-import sys
+
 import numpy as np
-from .utilities import check_alpha
+from pyfod.utilities import check_alpha
+from pyfod.utilities import check_function
 
 
 class RiemannSum(object):
@@ -15,38 +16,38 @@ class RiemannSum(object):
     def __init__(self, N=5, start=0.0, finish=1.0, alpha=0.0, f=None):
         self.description = 'Riemann-Sum'
         check_alpha(alpha=alpha)
-        self.grid = self.rs_grid(start, finish, N)
-        self.points = self.rs_points(grid=self.grid)
-        self.weights = self.rs_weights(grid=self.grid, alpha=alpha)
-
-    def integrate(self, f=None):
-        if f is None:
-            f = self.f
-        if f is None:
-            sys.exit('No function defined... provide function f')
-        return (self.weights*f(self.points)).sum()
-
-    @classmethod
-    def rs_grid(cls, start, finish, N):
-        return np.linspace(start=start, stop=finish, num=N)
-
-    @classmethod
-    def rs_points(cls, grid):
-        jj = grid.size - 1
-        return (grid[1:jj+1] + grid[0:jj])/2
-
-    @classmethod
-    def rs_weights(cls, grid, alpha=0.0):
-        jj = grid.size - 1
-        term2 = (grid[jj] - grid[1:jj+1])**(1-alpha)
-        term3 = (grid[jj] - grid[0:jj])**(1-alpha)
-        return -1/(1-alpha)*(term2 - term3)
+        self.alpha = alpha
+        self.f = f
+        self.grid = self._rs_grid(start, finish, N)
+        self.points = self._rs_points(grid=self.grid)
+        self.weights = self._rs_weights(grid=self.grid, alpha=alpha)
 
     def update_weights(self, alpha=None):
         if alpha is None:
             alpha = self.alpha
         self.alpha = alpha
-        self.weights = self.rs_weights(grid=self.grid, alpha=alpha)
+        self.weights = self._rs_weights(grid=self.grid, alpha=alpha)
+
+    def integrate(self, f=None):
+        f = check_function(f, self.f)
+        self.f = f
+        return (self.weights*f(self.points)).sum()
+
+    @classmethod
+    def _rs_grid(cls, start, finish, N):
+        return np.linspace(start=start, stop=finish, num=N)
+
+    @classmethod
+    def _rs_points(cls, grid):
+        jj = grid.size - 1
+        return (grid[1:jj+1] + grid[0:jj])/2
+
+    @classmethod
+    def _rs_weights(cls, grid, alpha=0.0):
+        jj = grid.size - 1
+        term2 = (grid[jj] - grid[1:jj+1])**(1-alpha)
+        term3 = (grid[jj] - grid[0:jj])**(1-alpha)
+        return -1/(1-alpha)*(term2 - term3)
 
 
 if __name__ == '__main__':  # pragma: no cover
