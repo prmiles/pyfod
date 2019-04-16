d15198832fc59e8957831c0722e4157c72fbc19c
prmiles <prmiles@ncsu.edu>
2019-04-16 17:12:28 -0400

    updated grunlet method

--- pyfod/GrunwaldLetnikov.py
+++ pyfod/GrunwaldLetnikov.py
@@ -3,39 +3,26 @@
 import numpy as np
 import sympy as sp
 from pyfod.utilities import check_node_type
-from pyfod.utilities import check_value
+from pyfod.utilities import check_input
 
 
-class GrunwaldLetnikov:
-
-    def __init__(self, N=5, start=0.0, finish=1.0, alpha=0.0, f=None):
-        self.description = 'Grunwald-Letnikov Fractional Derivative'
-        N = check_node_type(N)
-        self.start = start
-        self.finish = finish
-        self.alpha = alpha
-        self.N = N
-        self.f = f
-
-    def evaluate(self, x, N=None, alpha=None, f=None):
+def grunwaldletnikov(x, f=None, N=5, start=0.0, finish=1.0, alpha=0.0):
         # Check user input
-        self._check_user_settings(N=N, alpha=alpha, f=f)
+        _check_user_settings(N=N, alpha=alpha, f=f)
+        N = check_node_type(N)
         # Evaluate fractional derivative
         fd = 0.0
-        self.h = (self.finish - self.start)/(self.N)
-        for m in range(0, self.N):
-            tmp = (-1.0)**m * sp.binomial(self.alpha, m) * (
-                    self.f(x + (self.alpha - m)*self.h))
+        h = (finish - start)/(N)
+        for m in range(0, N):
+            tmp = (-1.0)**m * sp.binomial(alpha, m) * (
+                    f(x + (alpha - m)*h))
             fd += tmp
-#            print('{} of {}: {}'.format(m + 1, self.N,
-#                  fd/(self.h**self.alpha)))
-        return fd/(self.h**self.alpha)
+        return fd/(h**alpha)
 
-    def _check_user_settings(self, N, alpha, f):
-        self.f = check_value(f, self.f, varname='f')
-        self.alpha = check_value(alpha, self.alpha, varname='alpha')
-        self.N = check_value(N, self.N, varname='N')
-        self.N = check_node_type(self.N)
+def _check_user_settings(N, alpha, f):
+    check_input(f, varname='f')
+    check_input(alpha, varname='alpha')
+    check_input(N, varname='N')
 
 
 if __name__ == '__main__':  # pragma: no cover
@@ -63,7 +50,6 @@ if __name__ == '__main__':  # pragma: no cover
 #    print('D^{}[exp(2t)] = {} ({})'.format(alpha, out, 7.95224))
     # Test alpha = 0.9
     alpha = 0.9
-    GL = GrunwaldLetnikov(N=m, f=fspexp, alpha=alpha,
+    out = grunwaldletnikov(x=finish, N=m, alpha=alpha,
                           start=start, finish=finish)
-    out = GL.evaluate(x=finish)
     print('D^{}[exp(2t)] = {} ({})'.format(alpha, out, 13.8153))
