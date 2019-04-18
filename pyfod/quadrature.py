987b30d1b128226b5e8f2aeafc257197fcdfefd2
Graham Pash <root@graham-pc.localdomain>
2019-04-17 23:49:03 -0400

    update Gauss-Legendre to allow for user-defined deg-point quadrature

--- pyfod/quadrature.py
+++ pyfod/quadrature.py
@@ -10,19 +10,21 @@ from pyfod.utilities import check_node_type
 # ---------------------
 class GaussLegendre:
 
-    def __init__(self, N=5, start=0.0, finish=1.0,
+    def __init__(self, Ndom=5, deg=4, start=0.0, finish=1.0,
                  alpha=0.0, f=None, singularity=None):
         self.description = 'Gaussian-Legendre Quadrature'
         check_alpha(alpha)
-        N = check_node_type(N)
-        h = (finish - start)/N
+        Ndom = check_node_type(Ndom)
+        deg = check_node_type(deg)
+        h = (finish - start)/Ndom
         self.alpha = alpha
         self.f = f
-        self.N = N
+        self.Ndom = Ndom
+        self.deg = deg
         self.finish = finish
         self.singularity = check_singularity(singularity, self.finish)
-        self.points = self._gauss_points(N=N, h=h, start=start)
-        self.weights = self._gauss_weights(N=N, h=h)
+        self.points = self._gauss_points(Ndom=Ndom, deg=deg, h=h, start=start)
+        self.weights = self._gauss_weights(Ndom=Ndom, deg=deg, h=h)
         self.initial_weights = self.weights.copy()
         self.update_weights(alpha=alpha)
 
@@ -40,49 +42,40 @@ class GaussLegendre:
         return (self.weights*f(self.points)).sum()
 
     @classmethod
-    def _base_gauss_points(cls):
-
+    def _base_gauss_points(cls, deg):
         # base points
-        gpts = np.zeros([4])
-        gpts[0] = (1/2) - np.sqrt(15+2*np.sqrt(30))/(2*np.sqrt(35))
-        gpts[1] = (1/2) - np.sqrt(15-2*np.sqrt(30))/(2*np.sqrt(35))
-        gpts[2] = (1/2) + np.sqrt(15-2*np.sqrt(30))/(2*np.sqrt(35))
-        gpts[3] = (1/2) + np.sqrt(15+2*np.sqrt(30))/(2*np.sqrt(35))
+        gpts = np.polynomial.legendre.leggauss(deg)[0]
         return gpts
 
     @classmethod
-    def _base_gauss_weights(cls, h):
-        # define the Gauss weights for a four point quadrature rule
-        w = np.zeros([4])
-        w[0] = 49*h/(12*(18 + np.sqrt(30)))
-        w[1] = 49*h/(12*(18 - np.sqrt(30)))
-        w[2] = w[1]
-        w[3] = w[0]
+    def _base_gauss_weights(cls, deg, h):
+        # define the Gauss weights for a deg-point quadrature rule
+        w = np.polynomial.legendre.leggauss(deg)[1]*h
         return w
 
     @classmethod
-    def _interval_gauss_points(cls, base_gpts, N, h, start):
-        # determines the Gauss points for all N intervals.
-        Gpts = np.zeros([4*N])
-        for gct in range(N):
-            for ell in range(4):
-                Gpts[(gct)*4 + ell] = ((gct)*h
-                                       + base_gpts[ell]*h + start)
+    def _interval_gauss_points(cls, base_gpts, Ndom, deg, h, start):
+        # determines the Gauss points for all Ndom intervals.
+        Gpts = np.zeros([deg*Ndom])
+        for gct in range(Ndom):
+            for ell in range(deg):
+                Gpts[(gct)*deg + ell] = ((gct)*h
+                                         + base_gpts[ell]*h + start)
         return Gpts
 
-    def _gauss_points(self, N, h, start):
+    def _gauss_points(self, Ndom, deg, h, start):
         # base points
-        gpts = self._base_gauss_points()
-        # determines the Gauss points for all N intervals.
-        Gpts = self._interval_gauss_points(gpts, N, h, start)
+        gpts = self._base_gauss_points(deg)
+        # determines the Gauss points for all Ndom intervals.
+        Gpts = self._interval_gauss_points(gpts, Ndom, deg, h, start)
         return Gpts
 
-    def _gauss_weights(self, N, h):
-        # determine the Gauss weights for a four point quadrature rule
-        w = self._base_gauss_weights(h)
+    def _gauss_weights(self, Ndom, deg, h):
+        # determine the Gauss weights for a deg-point quadrature rule
+        w = self._base_gauss_weights(deg, h)
         # copy the weights to form a vector for all N intervals
         weights = w.copy()
-        for gct in range(N-1):
+        for gct in range(Ndom-1):
             weights = np.concatenate((weights, w))
         return weights
 
