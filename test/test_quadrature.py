b5d006ee7fcab58f5fbd540d20431f5bed4cf7c8
Graham Pash <root@graham-pc.localdomain>
2019-04-18 00:27:08 -0400

    update Gauss-Legendre tests

--- test/test_quadrature.py
+++ test/test_quadrature.py
@@ -12,7 +12,7 @@ class GLegTesting(unittest.TestCase):
         return np.exp(2*t)
 
     def test_init(self):
-        GL = qm.GaussLegendre(N=10, start=1.0, finish=12.0)
+        GL = qm.GaussLegendre(Ndom=10, deg=6, start=1.0, finish=12.0)
         attributes = ['points', 'weights', 'f', 'alpha', 'singularity',
                       'initial_weights', 'description']
         for att in attributes:
@@ -20,14 +20,14 @@ class GLegTesting(unittest.TestCase):
                             msg=str('Missing {} attribute'.format(att)))
 
     def test_update_weights(self):
-        GLeg = qm.GaussLegendre(N=10, start=1.0, finish=12.0)
+        GLeg = qm.GaussLegendre(N=10, deg=3, start=1.0, finish=12.0)
         GLeg.weights = []
         GLeg.update_weights(alpha=0.0)
         self.assertTrue(isinstance(GLeg.weights, np.ndarray),
                         msg='Weights should be updated.')
 
     def test_integrate(self):
-        GLeg = qm.GaussLegendre(N=10, start=0.0, finish=1.0)
+        GLeg = qm.GaussLegendre(Ndom=10, deg=3, start=0.0, finish=1.0)
         a = GLeg.integrate(f=self.f)
         self.assertTrue(isinstance(a, float), msg='Expect float')
 
@@ -36,8 +36,8 @@ class GLegTesting(unittest.TestCase):
 class BaseGaussPoints(unittest.TestCase):
 
     def test_base_gauss_points(self):
-        GQ = qm.GaussLegendre(N=10, start=1.0, finish=12.0)
-        gpts = GQ._base_gauss_points()
+        GQ = qm.GaussLegendre(Ndom=10, deg=4, start=1.0, finish=12.0)
+        gpts = GQ._base_gauss_points(deg=4)
         mlgpts = np.array([0.069431844202974, 0.330009478207572,
                            0.669990521792428, 0.930568155797026])
         self.assertEqual(gpts.size, 4, msg='Expect 4 elements')
@@ -52,8 +52,8 @@ class BaseGaussPoints(unittest.TestCase):
 class BaseGaussWeights(unittest.TestCase):
 
     def test_base_gauss_weights(self):
-        GQ = qm.GaussLegendre(N=10, start=1.0, finish=12.0)
-        gwts = GQ._base_gauss_weights(h=0.1)
+        GQ = qm.GaussLegendre(N=10, deg=4, start=1.0, finish=12.0)
+        gwts = GQ._base_gauss_weights(deg=4, h=0.1)
         mlgwts = np.array([0.017392742256873, 0.032607257743127,
                            0.032607257743127, 0.017392742256873]).T
         self.assertEqual(gwts.size, 4, msg='Expect 4 elements')
@@ -67,10 +67,10 @@ class BaseGaussWeights(unittest.TestCase):
 class IntervalGaussPoints(unittest.TestCase):
 
     def test_interval_gauss_points(self):
-        GQ = qm.GaussLegendre(N=10, start=1.0, finish=12.0)
+        GQ = qm.GaussLegendre(Ndom=10, deg=4, start=1.0, finish=12.0)
         igp = GQ._interval_gauss_points(
-                base_gpts=GQ._base_gauss_points(),
-                N=4, h=0.1, start=0.)
+                base_gpts=GQ._base_gauss_points(deg=4),
+                Ndom=4, deg=4, h=0.1, start=0.)
         self.assertEqual(igp.shape, (4*4,), msg='Expect shape = (16,)')
 
 
@@ -211,12 +211,12 @@ class Integrate_with_extended(unittest.TestCase):
     def test_with_f(self):
         GQ = qm.GaussLaguerre()
         a = GQ.integrate(f=self.fsp)
-        self.assertTrue(isinstance(a,float), msg='Expect float return')
+        self.assertTrue(isinstance(a, float), msg='Expect float return')
 
     def test_with_alpha(self):
         GQ = qm.GaussLaguerre()
         a = GQ.integrate(f=self.fsp)
-        self.assertTrue(isinstance(a,float), msg='Expect float return')
+        self.assertTrue(isinstance(a, float), msg='Expect float return')
 
 
 # --------------------------
